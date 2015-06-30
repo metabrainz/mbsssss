7688c9555dc809ed5d57cdcb0f4934811f0deae7
jeffweeksio <jeff@jeffweeks.io>
2015-06-30 12:02:31 -0400

    bulk commit: I will develop better habits from now on.

--- index/src/main/bin/buildindex.sh
+++
@@ -1,2 +0,0 @@
-java -Xmx512M -jar index-2.0-SNAPSHOT-jar-with-dependencies.jar -d musicbrainz_db -u musicbrainz_user --indexes-dir /Users/paul/musicbrainzindexes
-
--- index/src/main/java/org/musicbrainz/search/LuceneVersion.java
+++
@@ -1,8 +0,0 @@
-package org.musicbrainz.search;
-
-import org.apache.lucene.util.Version;
-
-public interface LuceneVersion {
-    public static final Version LUCENE_VERSION = Version.LUCENE_5_2_0;
-
-}
